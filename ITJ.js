043b14bfb6bd3cd7d2e6e6233c54ef3b100c9a88
Scholar <scholar@paradigm.local>
2023-10-31 14:57:02 -0600

    intro to js

---
+++ ITJ.js
@@ -0,0 +1,28 @@
+const myName = "Mr Hunt"
+
+let favColor = "Blue"
+
+favcolor = "Red"
+
+console.log(facColor)
+
+let age = 30
+
+mainperson = {
+    name: "Mr Hunt",
+    age: 30,
+    favColor: "Blue"
+    favFoods: ["steak", "ice cream", "potatoes"]
+}
+
+favthings = [2, "movies"]
+
+const isAlive = true 
+
+function describePerson(person) {
+    alert("MISSING PERSON ALERT: " + person.name)
+    console.log(`MISSING PEROSON ALERT: ${person.name} MISSING SINCE 2022,
+     WEARING TATTERED OVERALLS`)
+}
+
+describePerson(mainPerson)
