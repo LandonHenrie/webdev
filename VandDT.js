e3e26e2934a8126c6c8274f2bf66b220a9421c21
Scholar <scholar@paradigm.local>
2023-11-07 14:31:45 -0700

    variables

---
+++ VandDT.js
@@ -0,0 +1,56 @@
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Numbers:
+let hight = 81;
+let mass = 22;
+
+// Numbers:
+let age = 22
+let ages = 32
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Strings:
+let shoe = "Nike"
+let computer = "Alienware"
+
+// Strings:
+let monkey = "Black"
+let datas = "Number"
+
+// Booleans
+if (age<22) {
+    decodeURIComponent.getelementByid("title").innerHTML = "awsome";
+}
+
+// Booleans
+if (age>32) {
+    decodeURIComponent.getelementByid("incse").innerHTML = "WAAAAA";
+}
+
+// Booleans
+if (age=12) {
+    decodeURIComponent.getelementByid("MR").innerHTML = "HHHHEE";
+}
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Array object:
+var computers = ["Alienware","Dell","Chrome" ]
+
+// Array object:
+var shoes = ["Nike","Adiddas","Puma"]
+
+// Date object:
+const date = new Date("2022-03-25");
+
+// Date object: 
+var data = new DataTransfer("Number, Buisness")
+
+// Date object: 
+var Monkey = new Monkey("Brown,Black")
