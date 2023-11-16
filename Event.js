630c4af20557b610e7dca99fb68bcd90a89c17cf
Scholar <scholar@paradigm.local>
2023-11-16 14:40:40 -0700

    event listeners

---
+++ Event.js
@@ -0,0 +1,51 @@
+document.getElementById("greeting").addEventListener("click", function() {
+    document.getElementById("greeting").innerHTML = "We Not Me";
+});
+
+document.getElementById("greeting").addEventListener("dblclick", function() {
+    document.getElementById("greeting").innerHTML = "Wee";
+});
+
+document.getElementById("color").addEventListener("mouseover", function () {
+    document.getElementById("color").style.backroundColor = "red";
+    document.getElementById("color").style.height = "500px";
+    document.getElementById("color").style.width = "500px";
+    document.getElementById("color").style.borderRadius = "50%";
+});
+
+document.getElementById("color").addEventListener("mouseout", function () {
+    document.getElementById("color").style.backroundColor = "red";
+    document.getElementById("color").style.height = "200px";
+    document.getElementById("color").style.width = "200px";
+    document.getElementById("color").style.borderRadius = "0%";
+});
+
+documentbutton.addEventListener("click", myFunction); {
+    document.getElementById("button").innerHTML = "red";
+};
+
+object.ondrag = function(){myScript}; {
+    object.addEventListener("drag", myScript);
+}
+
+object.onfocus = function(){myScript}; {
+    object.addEventListener("focus", myScript);
+}
+
+object.oninput = function(){myScript};{
+    object.addEventListener("input", myScript);
+}
+
+object.onabort = function(){myScript}; {
+    object.addEventListener("abort", myScript);
+}
+
+object.oncut = function(){myScript}; {
+    object.addEventListener("cut", myScript);
+}
+
+
+
+
+
+
