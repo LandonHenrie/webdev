422e62692086e5627a9431407d2ca631e3eae75a
Scholar <scholar@paradigm.local>
2023-11-28 13:48:37 -0700

    simple budgeter

--- JSSB.js
+++ JSSB.js
@@ -1,3 +1,11 @@
 function expense() {
-    
+    taxes = document.getElementById(`taxes`).value;
+    Total Money = document.getElementById(`Total Expense`).value;
+    rest of money = document.getElementById('Rest OF Money').value
+
+    document.getElementById("fortune").innerHTML = `You will have ${numofChildren} children with ${partnername} and work as a ${job}`
 }
+
+document.getElementById("Money").addEventListener("click", function() {
+    document.getElementById("Money").innerHTML = "More Money";
+});
